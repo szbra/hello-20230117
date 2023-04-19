bc4a4b8ea0298202bce2f10de189a957aef48340
Szymon Brandys <szymon.brandys@gmail.com>
2023-04-19 11:08:53 +0200

    Commit

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 
 
   
-const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service";
+const welcomeMessage = "Devops demo";
 const portMessage = "Application Running on port";
 
 const getWelcomeMessage = () => {
